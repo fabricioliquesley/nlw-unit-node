13d56cd8aa8500f0a7bd4d07248e6f4543092c5e
Fabricio <fabricioliquesley15@gmail.com>
2024-04-02 19:00:42 -0300

    feat: create the generate-slug function to create a slug for the event

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import { z } from "zod";
 import { PrismaClient } from "@prisma/client";
+import { generateSlug } from "./utils/generate-slug.js";
 
 const app = fastify();
 
@@ -8,21 +9,35 @@ const prisma = new PrismaClient({
   log: ["query"],
 });
 
-app.post("/events", async (request, reply) => {
+app.post('/events', async (request, reply) => {
   const createEventSchema = z.object({
     title: z.string().min(4),
     details: z.string().nullable(),
     maximumAttendees: z.number().int().positive().nullable(),
   });
 
-  const data = createEventSchema.parse(request.body);
+  const { title, details, maximumAttendees } = createEventSchema.parse(
+    request.body
+  );
+
+  const slug = generateSlug(title);
+
+  const eventWithSlug = prisma.event.findUnique({
+    where: {
+      slug,
+    },
+  });
+
+  if (eventWithSlug !== null) {
+    throw new Error("Another event with same title already exists.");
+  }
 
   const event = await prisma.event.create({
     data: {
-      title: data.title,
-      details: data.details,
-      maximumAttendees: data.maximumAttendees,
-      slug: new Date().toISOString(),
+      title: title,
+      details: details,
+      maximumAttendees: maximumAttendees,
+      slug,
     },
   });
 
