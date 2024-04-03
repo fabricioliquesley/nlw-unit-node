e6ba119523e70310f089f29fa21c23b21c0b7dbc
Fabricio <fabricioliquesley15@gmail.com>
2024-04-03 12:26:30 -0300

    feat: register checkIn and getEventAttendees routes in the server file

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import { createEvent } from "./routes/create-events.js";
 import { registerForEvent } from "./routes/register-for-event.js";
 import { getEvent } from "./routes/get-event.js";
 import { getAttendeeBadge } from "./routes/get-attendee-badge.js";
+import { checkIn } from "./routes/check-in.js";
+import { getEventAttendees } from "./routes/get-event-attendees.js";
 
 const app = fastify();
 
@@ -17,6 +19,8 @@ app.register(createEvent);
 app.register(registerForEvent);
 app.register(getEvent);
 app.register(getAttendeeBadge);
+app.register(checkIn);
+app.register(getEventAttendees)
 
 app.listen({ port: 3333 }).then(() => {
   console.log("API listening on port 3333");
