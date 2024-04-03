e3d9d7f7798b1193d6541a748e31f0f304020a56
Fabricio <fabricioliquesley15@gmail.com>
2024-04-02 22:07:56 -0300

    feat: register the getAttendee, getEvent and registerAttendee routes in the server file

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,9 @@ import {
   validatorCompiler,
 } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-events.js";
+import { registerForEvent } from "./routes/register-for-event.js";
+import { getEvent } from "./routes/get-event.js";
+import { getAttendeeBadge } from "./routes/get-attendee-badge.js";
 
 const app = fastify();
 
@@ -11,6 +14,9 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent);
+app.register(registerForEvent);
+app.register(getEvent);
+app.register(getAttendeeBadge);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("API listening on port 3333");
